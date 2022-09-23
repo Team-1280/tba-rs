4d4fa1c74752c65281d3f21724d1578155b18346
Bendi11 <bkliebmann@gmail.com>
2022-09-22 22:22:19 -0700

    Added model types for teams

--- src/ctx/mod.rs
+++ src/ctx/mod.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use reqwest::{Client, ClientBuilder, header::HeaderMap};
 
 use crate::Error;
@@ -11,13 +13,19 @@ pub struct Context {
 
 impl Context {
     /// Create a new context with the given API key
-    pub fn new(tba_auth_key: impl AsRef<str>) -> Result<Self, Error> {
+    pub fn authenticate(tba_auth_key: impl AsRef<str>) -> Result<Self, Error> {
         let mut headers = HeaderMap::new();
-        headers.insert("X-TBA-Auth-Key", tba_auth_key.as_ref().parse()?);
+        headers.insert(
+            "X-TBA-Auth-Key",
+            tba_auth_key
+                .as_ref()
+                .parse()?
+        );
 
         Ok(Context {
             client: ClientBuilder::new()
                 .default_headers(headers)
+                .connect_timeout(Duration::from_secs(30))
                 .build()?,
         })
     }
--- src/model/team.rs
+++ src/model/team.rs
@@ -1,1 +1,46 @@
+use reqwest::Url;
+use serde::Deserialize;
 
+use super::id::TeamNumber;
+
+
+#[derive(Clone, Deserialize)]
+pub struct SimpleTeam {
+    pub key: String,
+    pub team_number: TeamNumber,
+    pub nickame: Option<String>,
+    pub name: String,
+    pub city: Option<String>,
+    pub state_prov: Option<String>,
+    pub country: Option<String>,
+}
+
+#[derive(Clone, Deserialize)]
+pub struct Team {
+    simple: SimpleTeam,
+    address: Option<String>,
+    postal_code: Option<String>,
+    gmaps_place_id: Option<String>,
+    gmaps_url: Option<Url>,
+    lat: Option<f64>,
+    lng: Option<f64>,
+    location_name: Option<String>,
+    website: Option<Url>,
+    rookie_year: Option<u16>,
+    motto: Option<String>,
+    home_championship: HomeChampionshipsList,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct HomeChampionshipsList(Vec<u16, String>);
+
+impl AsRef<SimpleTeam> for Team {
+    fn as_ref(&self) -> &SimpleTeam {
+        &self.simple
+    }
+}
+impl AsMut<SimpleTeam> for Team {
+    fn as_mut(&mut self) -> &mut SimpleTeam {
+        &mut self.simple
+    }
+}
